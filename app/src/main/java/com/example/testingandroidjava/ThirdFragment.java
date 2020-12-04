cad25f76d765686c59adeac54d7960aa726818c9
NurshahidatulAdaniShahrulnizam <nurshahidatuladani@gmail.com>
2020-12-04 16:58:25 +0800

    first commit

---
+++ app/src/main/java/com/example/testingandroidjava/ThirdFragment.java
@@ -0,0 +1,85 @@
+package com.example.testingandroidjava;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ThirdFragment extends Fragment {
+    private TextView textView;
+    private RequestQueue requestQueue;
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = (TextView) view.findViewById(R.id.text_http);
+        Button button = (Button) view.findViewById(R.id.get_http_button);
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+        view.findViewById(R.id.button_third).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_thirdFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    public void jsonParse(){
+        String url = "https://jsonplaceholder.typicode.com/albums/1";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    int userId = response.getInt("userId");
+                    int id = response.getInt("id");
+                    String title = response.getString("title");
+                    Log.i("JSON",title);
+                    textView.setText(String.valueOf(userId) + " " + String.valueOf(id) + " " + title);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.e("JSON","Error");
+            }
+        });
+
+        requestQueue.add(request);
+    }
+}
