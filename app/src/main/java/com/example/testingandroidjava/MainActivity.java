0fe830753ab471e0c13d7059828bea40722b1993
NurshahidatulAdaniShahrulnizam <nurshahidatuladani@gmail.com>
2020-12-28 16:18:11 +0800

    speed dial

--- app/src/main/java/com/example/testingandroidjava/MainActivity.java
+++ app/src/main/java/com/example/testingandroidjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.testingandroidjava;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.testingandroidjava.helper.PermissionsHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+import com.leinardi.android.speeddial.SpeedDialView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private PermissionsHelper permissionsHelper;
@@ -29,12 +33,29 @@ public class MainActivity extends AppCompatActivity {
 
         checkPermissions();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        SpeedDialView speedDialView = findViewById(R.id.speedDial);
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_no_label, R.drawable.ic_code_scanner_auto_focus_off).setLabel("Testing").create());
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                switch (actionItem.getId()){
+                    case R.id.fab_no_label :
+                        Toast.makeText(MainActivity.this,"No label action clicked!\nClosing with animation",Toast.LENGTH_SHORT).show();
+                        speedDialView.close(); // To close the Speed Dial with animation
+                        return true; // false will close it without animation
+                }
+
+                return false;
             }
         });
     }
